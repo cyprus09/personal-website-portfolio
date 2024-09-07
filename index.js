97044b0e8012ba013da6ded49c950fec093fc002
mayankpallai <mayankpallai@gmail.com>
2024-09-08 06:12:27 +0800

    dark mode enable

---
+++ index.js
@@ -0,0 +1,14 @@
+const modeToggle = document.getElementById('modeToggle');
+const body = document.body;
+const icon = modeToggle.querySelector('i');
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
